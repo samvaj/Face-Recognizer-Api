3f61d0b07294461eb62fcbdd8f0db61fd5aee85a
Rahul <rrangith@edu.uwaterloo.ca>
2018-03-03 15:16:40 -0500

    testing

--- controllers/image.js
+++ controllers/image.js
@@ -9,6 +9,7 @@ const handleApiCall = (req, res) => {
     app.models.predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
     .then(data => {
         res.json(data);
+        console.log('success')
     })
     .catch(err => res.status(400).json('Unable to Work With API'))
 }
